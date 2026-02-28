7421eb86b0cb664a1c87a2770a782f0d341e1079
elnaske <naske.elias@outlook.com>
2026-02-27 22:11:49 -0500

    Basic CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+
+struct Config {
+    infile: String,
+    outfile: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str>{
+        if args.len() < 4 {
+            return Err("not enough arguments");
+        }
+        if args.len() > 4 {
+            return Err("too many arguments")
+        }
+
+        let mut output_next = false;
+        let mut output = String::new();
+        let mut input = String::new();
+
+        for arg in &args[1..] {
+            if output_next {
+                output = arg.clone();
+                output_next = false;
+                continue;
+            }
+
+            if arg == "-o" {
+                output_next = true;
+                continue;
+            }
+            input = arg.clone();
+        }
+        Ok(Config { infile: input, outfile: output })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+    let cfg = Config::build(&args).unwrap();
+    println!("Input: {}", cfg.infile);
+    println!("Output: {}", cfg.outfile);
 }
