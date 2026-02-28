1fd8de2a9a0f418d66d11dd06dd1f015ca1c985f
elnaske <naske.elias@outlook.com>
2026-02-28 15:44:38 -0500

    Add gcc-based driver

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
+use std::fs::remove_file;
+use std::process::Command;
 
 struct Config {
     infiles: Vec<String>,
     outfile: Option<String>,
 }
+
+// TODO: better file handling
 
 impl Config {
     fn new() -> Config {
@@ -37,8 +41,42 @@ impl Config {
     }
 }
 
+fn compile(cfg: Config) {
+    let binding = cfg.outfile.clone().expect("no output file");
+    let output = binding.as_str();
+    let preprocessor_output = output.to_owned() + ".i";
+    let assembly_output = output.to_owned() + ".s";
+
+    // preprocessor
+    Command::new("gcc")
+        .args([
+            "-E",
+            "-P",
+            &cfg.infiles.join(" "),
+            "-o",
+            &preprocessor_output,
+        ])
+        .output()
+        .expect("preprocessing failed");
+
+    // compiler 
+    // (using gcc for now for testing)
+    Command::new("gcc")
+        .args(["-S", &preprocessor_output, "-o", &assembly_output])
+        .output()
+        .expect("compilation failed");
+
+    // assembler and linker
+    Command::new("gcc")
+        .args([&assembly_output, "-o", &output])
+        .output()
+        .expect("assembling/linking failed");
+
+    remove_file(preprocessor_output).expect("failed to remove preprocessed file");
+    remove_file(assembly_output).expect("failed to remove assembly file");
+}
+
 fn main() {
     let cfg = Config::parse().unwrap();
-    dbg!(cfg.infiles);
-    println!("Output: {}", cfg.outfile.unwrap());
+    compile(cfg);
 }
