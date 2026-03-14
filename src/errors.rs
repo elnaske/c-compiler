f5806a9dd92726a227e42d030a7f1e94fff20e1b
elnaske <naske.elias@outlook.com>
2026-03-13 21:32:45 -0400

    Update error handling in lexer

--- src/errors.rs
+++ src/errors.rs
@@ -1,34 +1,34 @@
-use std::path::PathBuf;
+// use std::path::PathBuf;
 
 use crate::lexer::Token;
 
-struct CompilerError {
-    kind: ErrorKind,
-    filename: PathBuf,
-    line_string: String,
-    row: usize,
-    col: usize,
+pub struct CompilerError {
+    pub kind: ErrorKind,
+    pub filename: String,
+    pub line_string: String,
+    pub row: usize,
+    pub col: usize,
 }
 impl CompilerError {
-    pub fn new(kind: ErrorKind, filename: PathBuf, line_string: String, row: usize, col: usize) -> Self {
+    pub fn new(kind: ErrorKind, filename: String, line_string: String, row: usize, col: usize) -> Self {
         CompilerError { kind, filename, line_string, row, col }
     }
 
-    fn print(&self) {
-        eprintln!("{}:{}:{}: error: {}", self.filename.display(), self.row, self.col, self.kind.to_string());
+    pub fn print(&self) {
+        eprintln!("{}:{}:{}: error: {}", self.filename, self.row, self.col, self.kind.to_string());
         // TODO: align these two lines better
         eprintln!("{:>5} | {}", self.row, self.line_string);
-        eprintln!("{:>5} | {:<width$}", "", "^", width=self.col);
+        eprintln!("{:>5} | {:>width$}", "", "^", width=self.col);
     }
 }
 
-enum ErrorKind {
+pub enum ErrorKind {
     InvalidCharacter,
     InvalidIntSuffix,
     LeftoverTokens,
     Expected { expected: Token, actual: Token },
-    CLIInvalidFlag { flag: String },
-    CLIMissingArg { flag: String, expected: String },
+    // CLIInvalidFlag { flag: String },
+    // CLIMissingArg { flag: String, expected: String },
 }
 impl ErrorKind {
     fn to_string(&self) -> String {
@@ -39,10 +39,10 @@ impl ErrorKind {
             ErrorKind::Expected { expected, actual } => {
                 format!("Expected {:?}, found {:?}", expected, actual)
             }
-            ErrorKind::CLIInvalidFlag { flag } => format!("Invalid compiler flag `{}`", flag),
-            ErrorKind::CLIMissingArg { flag, expected } => {
-                format!("Expected {} after `{}`", expected, flag)
-            }
+            // ErrorKind::CLIInvalidFlag { flag } => format!("Invalid compiler flag `{}`", flag),
+            // ErrorKind::CLIMissingArg { flag, expected } => {
+            //     format!("Expected {} after `{}`", expected, flag)
+            // }
         }
     }
 }
--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,7 @@
+use std::path::PathBuf;
+
+use crate::errors::{CompilerError, ErrorKind};
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Identifier(String),
@@ -8,6 +12,7 @@ pub enum Token {
     OpenBrace,
     CloseBrace,
     Semicolon,
+    Eof,
 }
 
 #[derive(Debug, PartialEq)]
@@ -34,11 +39,18 @@ impl Keyword {
 pub struct Lexer<'a> {
     input: &'a [u8],
     pos: usize,
+    row: usize,
+    col: usize,
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &'a [u8]) -> Self {
-        Lexer { input, pos: 0 }
+        Lexer {
+            input,
+            pos: 0,
+            row: 1,
+            col: 1,
+        }
     }
 
     pub fn reset(&mut self) {
@@ -46,43 +58,52 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn get_tokens(&mut self) -> Vec<Token> {
-        // parse tokens (self.next_token) until EOF
         let mut tokens = Vec::<Token>::new();
-        while let Some(token) = self.next_token() {
-            tokens.push(token);
+        loop {
+            match self.next_token() {
+                Ok(token) if token == Token::Eof => break,
+                Ok(token) => tokens.push(token),
+                Err(e) => {e.print(); panic!()},
+            }
         }
         self.reset();
         tokens
     }
 
-    fn next_token(&mut self) -> Option<Token> {
+    fn next_token(&mut self) -> Result<Token, CompilerError> {
         self.skip_whitespace();
 
         match self.peek() {
-            Some(b'a'..=b'z' | b'A'..=b'Z' | b'_') => Some(self.lex_identifier()),
-            Some(b'0'..=b'9') => Some(self.lex_constant()),
+            Some(b'a'..=b'z' | b'A'..=b'Z' | b'_') => Ok(self.lex_identifier()),
+            Some(b'0'..=b'9') => self.lex_constant(),
             Some(b'(') => {
                 self.advance();
-                Some(Token::OpenParenthesis)
+                Ok(Token::OpenParenthesis)
             }
             Some(b')') => {
                 self.advance();
-                Some(Token::CloseParenthesis)
+                Ok(Token::CloseParenthesis)
             }
             Some(b'{') => {
                 self.advance();
-                Some(Token::OpenBrace)
+                Ok(Token::OpenBrace)
             }
             Some(b'}') => {
                 self.advance();
-                Some(Token::CloseBrace)
+                Ok(Token::CloseBrace)
             }
             Some(b';') => {
                 self.advance();
-                Some(Token::Semicolon)
+                Ok(Token::Semicolon)
             }
-            None => None,
-            _ => panic!("illegal character"),
+            None => Ok(Token::Eof),
+            _ => Err(CompilerError {
+                kind: ErrorKind::InvalidCharacter,
+                filename: "TODO.c".to_string(),
+                line_string: "TODO".to_string(),
+                row: self.row,
+                col: self.col,
+            }),
         }
     }
 
@@ -91,6 +112,12 @@ impl<'a> Lexer<'a> {
     }
 
     fn advance(&mut self) {
+        if self.peek() == Some(b'\n') {
+            self.row += 1; // this doesn't work right; idk why
+            self.col = 1;
+        } else {
+            self.col += 1
+        }
         self.pos += 1;
     }
 
@@ -123,24 +150,31 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn lex_constant(&mut self) -> Token {
+    fn lex_constant(&mut self) -> Result<Token, CompilerError> {
         let start = self.pos;
 
         loop {
             match self.peek() {
                 Some(b'0'..=b'9') => self.advance(),
                 Some(b'a'..=b'z' | b'A'..=b'Z' | b'_') => {
-                    panic!("invalid suffix on integer constant")
+                    // panic!("invalid suffix on integer constant")
+                    return Err(CompilerError{
+                        kind: ErrorKind::InvalidIntSuffix,
+                        filename: "TODO.c".to_string(),
+                        line_string: "TODO".to_string(),
+                        row: self.row,
+                        col: self.col,
+                    })
                 } // put this here to satisfy a test case, might change how this is handled in the future
                 _ => break,
             }
         }
-        Token::Constant(
+        Ok(Token::Constant(
             str::from_utf8(&self.input[start..self.pos])
                 .expect("Invalid UTF-8 sequence")
                 .parse()
                 .expect("Not a valid number"),
-        )
+        ))
     }
 }
 
